e25166858b2d2c35ae8e2568aae94272f4e6a943
Yurb <yavurb@gmail.com>
2023-05-11 21:27:27 -0400

    refactor(auth): change password validation's error message
    now the message describe the proper format for a strong password

--- src/auth/dtos/signup.dto.ts
+++ src/auth/dtos/signup.dto.ts
@@ -16,6 +16,12 @@ export class SingUpDto {
   @IsDefined()
   email: string;
 
-  @IsStrongPassword({ minLength: 10, minUppercase: 1, minSymbols: 1 })
+  @IsStrongPassword(
+    { minLength: 10, minUppercase: 1, minSymbols: 1 },
+    {
+      message:
+        'Password not strong enough, make sure it has at least, 10 characters, 1 uppercased letter and 1 symbol',
+    },
+  )
   password: string;
 }
