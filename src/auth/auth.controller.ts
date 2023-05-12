30c19b6d30b471a8718fb90020d7968e8edc4c65
Yurb <yavurb@gmail.com>
2023-05-11 21:24:36 -0400

    feat(auth): add signup route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+
+import { SingUpDto } from './dtos';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signup(@Body() signupInfo: SingUpDto) {
+    return this.authService.signup(signupInfo);
+  }
+}
