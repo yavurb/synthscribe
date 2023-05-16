d355666b5271cfa8b40b1460d7189f4875a243cd
Yurb <yavurb@gmail.com>
2023-05-15 21:43:46 -0400

    fix: delete AccountsModule from app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
-import { AccountsModule } from './accounts/accounts.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     AuthModule,
-    AccountsModule,
     ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
